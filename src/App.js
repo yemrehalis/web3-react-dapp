74373bdf7c4831e2241246f8c995e2346c5bd0fc
yemrehalis <yemrehalis@gmail.com>
2022-03-24 13:59:52 +0300

    fetch greeting function added to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { ethers } from 'ethers';
 import './App.css';
 
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+
+const greeterAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+
 function App() {
+  const [greeting, setGreetingValue] = useState()
+
+  async function requestAccount() {
+
+  }
+
+  async function fetchGreeting() {
+
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+
+  }
+
+  async function setGreeting() {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
