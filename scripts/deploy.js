ab0ba469ab763d47fd4b97be391d7ed3d763ad96
yemrehalis <yemrehalis@gmail.com>
2022-03-25 14:29:36 +0300

    functions for personal token added to App.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,10 +5,13 @@ async function main() {
 
   const Greeter = await hre.ethers.getContractFactory("Greeter");
   const greeter = await Greeter.deploy("Hello, Hardhat!");
-
   await greeter.deployed();
-
   console.log("Greeter deployed to:", greeter.address);
+
+  const Token = await hre.ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+  await token.deployed();
+  console.log("Token deployed to:", token.address);
 }
 
 
